c8ce830231d7ca32df781c3b826ab8226580203a
renahc <renatofelipeherrera.c@gmail.com>
2026-09-14 00:24:01 -0300

    fix(infra): use triple backslash escaping for Nginx variables in Terraform user_data

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -111,43 +111,43 @@ server {
     index index.html;
 
     location / {
-        try_files \\$uri \\$uri/ /index.html;
+        try_files \\\$uri \\\$uri/ /index.html;
     }
 
     location /api/v1/appointments {
         proxy_pass http://${var.backend_host}:8081;
-        proxy_set_header Host \\$host;
-        proxy_set_header X-Real-IP \\$remote_addr;
+        proxy_set_header Host \\\$host;
+        proxy_set_header X-Real-IP \\\$remote_addr;
     }
 
     location /api/v1/services {
         proxy_pass http://${var.backend_host}:8081;
-        proxy_set_header Host \\$host;
-        proxy_set_header X-Real-IP \\$remote_addr;
+        proxy_set_header Host \\\$host;
+        proxy_set_header X-Real-IP \\\$remote_addr;
     }
 
     location /api/v1/clients {
         proxy_pass http://${var.backend_host}:8082;
-        proxy_set_header Host \\$host;
-        proxy_set_header X-Real-IP \\$remote_addr;
+        proxy_set_header Host \\\$host;
+        proxy_set_header X-Real-IP \\\$remote_addr;
     }
 
     location /api/v1/stylists {
         proxy_pass http://${var.backend_host}:8082;
-        proxy_set_header Host \\$host;
-        proxy_set_header X-Real-IP \\$remote_addr;
+        proxy_set_header Host \\\$host;
+        proxy_set_header X-Real-IP \\\$remote_addr;
     }
 
     location /api/v1/auth {
         proxy_pass http://${var.backend_host}:8082;
-        proxy_set_header Host \\$host;
-        proxy_set_header X-Real-IP \\$remote_addr;
+        proxy_set_header Host \\\$host;
+        proxy_set_header X-Real-IP \\\$remote_addr;
     }
 
     location /api/v1/notifications {
         proxy_pass http://${var.backend_host}:8083;
-        proxy_set_header Host \\$host;
-        proxy_set_header X-Real-IP \\$remote_addr;
+        proxy_set_header Host \\\$host;
+        proxy_set_header X-Real-IP \\\$remote_addr;
     }
 }
 NGINX_CONF
