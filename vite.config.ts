d2a9de7eecf66ad36994069d0c4d5ed0771f62a8
renahc <renatofelipeherrera.c@gmail.com>
2026-09-14 02:15:35 -0300

    feat: habilitar SSL/HTTPS nativo con @vitejs/plugin-basic-ssl para compatibilidad con Azure AD

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    basicSsl(),
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
