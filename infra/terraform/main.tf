2ae754e22054efbe17472e7694e69b454cd9b0ae
renahc <renatofelipeherrera.c@gmail.com>
2026-09-14 02:23:24 -0300

    feat: habilitar soporte HTTPS (puerto 443, SSL en Nginx y Security Group Terraform) para servidor EC2

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -52,6 +52,14 @@ resource "aws_security_group" "frontend_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "HTTPS Access"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
